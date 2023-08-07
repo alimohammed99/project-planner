950b8f2727d655f6b3631ecdfb7d5e22c3a87b6c
Mohammed Ali <alimoh22062000@gmail.com>
2023-08-07 17:40:25 +0100

    Worked on toggling projects details and action icons

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,101 @@
+<template>
+    <div class="project">
+    
+        <div class="actions">
+    
+            <h1 title="Click to display project title" @click="toggleShowProjectsDetails" style="color: indigo">
+    
+                {{ allProjects.title }}
+    
+            </h1>
+    
+    
+    
+            <div class="icons">
+    
+                <span class="material-icons">edit</span>
+    
+    
+    
+                <span class="material-icons"> delete </span>
+    
+    
+    
+                <span class="material-icons"> done </span>
+    
+            </div>
+    
+        </div>
+    
+    
+    
+        <div v-if="showProjectsDetails" class="details">
+    
+            <p style="font-size: 25px; color: brown">
+    
+                <em>{{ allProjects.details }}</em>
+    
+            </p>
+    
+        </div>
+    
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['allProjects', 'bigDiv'],
+
+    data() {
+        return {
+            showProjectsDetails: false
+        }
+    },
+
+    mounted() {},
+
+    methods: {
+        // Projects details aren't gonna show unless User clicks on the projects titles and User can toggle btw hiding and showing the projects details.
+
+        toggleShowProjectsDetails() {
+            this.showProjectsDetails = !this.showProjectsDetails
+        }
+    }
+}
+</script>
+
+<style>
+/* I gave both the 'mother-div' and the 'child-div' same stylings, so it's gonna be like a double-styling in the interface. */
+
+.project,
+.projectDiv {
+    margin: 20px auto;
+    background: white;
+    padding: 10px 20px;
+    border-radius: 4px;
+    /*box-shadow: 7px 7px 3px rgba(128, 128, 128);*/
+    box-shadow: 10px 10px 3px rgba(36, 17, 17, 0.1);
+    border-left: 7px solid #e90074;
+}
+
+h1 {
+    cursor: pointer;
+}
+
+.actions{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+
+.material-icons{
+    font-size: 30px;
+    margin-left: 10px;
+    color: #bbb;
+    cursor: pointer;
+}
+
+.material-icons:hover {
+    color: #777;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,51 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- Wanna display the items ('projects') only if the project array has a length i.e, has data inside. Coz there's no point cycling through the array if it's empty. -->
+    <div v-if="projects.length">
+      <div class="projectDiv" v-for="project in projects" :key="project.id">
+        <!-- For loops in VUE must have a key attribute, and it must be unique for each item. That's why I picked the id. ID is unique. -->
+
+        <!-- I externalized this template into a separate component (reusable) component. This is because I'm not just gonna display only job title or something. Each project should have a bit more template. I could've done it here though. -->
+
+        <!-- This is the single reusable component to display the projects. -->
+        <!-- "project" is derived from the for-loop. -->
+        <SingleProject :allProjects="project" :bigDiv="projectDiv" />
+        <!-- "projectDiv" is the class name of this mother-div. So I'm sending it as a prop to the 'singleProject.vue' because I want to style it there. That shows css class names can be sent as props. -->
+      </div>
+    </div>
+
+    <div v-else>
+      <p style="font-size: 90px; color: tomato">Loading.....</p>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import SingleProject from "../components/SingleProject.vue";
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  components: { SingleProject },
+  data() {
+    return {
+      // Since I want to get the data from another place (json file) and use it here, the best place/time to enable/get it is inside the mounted() lifecycle. That's why when the component has been mounted to the DOM.
+      projects: [],
+    };
+  },
+  mounted() {
+    // I used Fetch API. I need to pass in the endpoint which was provided by the json server to get all the jobs. Now this is gonna fetch all the data for me and return them in json format.
+    // The below function is asynchronous and therefore returns a Promise.
+    fetch(" http://localhost:3000/projects")
+      // The below function fires a callback function after the above function is done.
+      // The below is necessary in order to actually get that data and pass into Javascript, I need to take Response.
+      // This is asynchronous and therefore also returns a Promise.
+      .then((res) => res.json())
+
+      // This is just to populate whatever data in the 'jobs' array inside "data".
+      .then((data) => (this.projects = data))
+
+      // This is needed incase there's any error with the asynchronous tasks up there.
+      .catch((err) => console.log(err.message));
+  },
+};
 </script>
